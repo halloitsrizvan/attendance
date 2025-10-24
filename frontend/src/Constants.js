e4080cf15fa2590294875cec863a919f5b7d356a
halloitsrizvan <clgrizvan@example.com>
2025-10-24 08:27:10 +0530

    api setted

--- frontend/src/Constants.js
+++ frontend/src/Constants.js
@@ -1,2 +1,2 @@
-export const API_PORT = "http://localhost:4000" 
+export const API_PORT = "https://attendance-5zbp.onrender.com" 
                         // || "https://attendance-5zbp.onrender.com"
