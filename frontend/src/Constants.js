ec91dd32c6f681721bbe166d34bf227674e4c48d
halloitsrizvan <clgrizvan@gmail.com>
2025-12-29 22:07:30 +0530

    fix: update API_PORT to point to the production server

--- frontend/src/Constants.js
+++ frontend/src/Constants.js
@@ -1,3 +1,3 @@
-export const API_PORT = 'http://localhost:4000';
+export const API_PORT = 'https://attendance-5zbp.onrender.com';
 // 'http://localhost:4000'
 // "https://attendance-5zbp.onrender.com" 
